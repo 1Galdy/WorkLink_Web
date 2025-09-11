999b39b2e1db50172709c9c8dd5bddfc513c0c25
1Galdy <117950402+1Galdy@users.noreply.github.com>
2025-09-11 15:55:20 +0200

    Mon site Responsive

--- src/features/featureHome/utils/Nav.jsx
+++ src/features/featureHome/utils/Nav.jsx
@@ -3,5 +3,10 @@
 export function download(e) {
     e.preventDefault();
 
-    window.location.href = '../../../../public/test.zip';
+    const link = document.createElement('a');
+        link.href = '/WorkLink.apk'; // doit pointer vers /public/WorkLink.apk
+        link.setAttribute('download', 'WorkLink.apk');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
 }
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['740d153688a2.ngrok-free.app', 'localhost']
+  }
 })
